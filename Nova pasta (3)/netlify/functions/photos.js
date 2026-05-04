f3cada027756557be7f0a58953d6794f7047fb75
masterptpt <pachecotiago250@gmail.com>
2026-05-04 23:20:08 +0100

    Delete Nova pasta (3) directory

--- Nova pasta (3)/netlify/functions/photos.js
+++
@@ -1,9 +0,0 @@
-exports.handler = async (event, context) => {
-  // Netlify Functions não mantém estado persistente
-  // As imagens ficam armazenadas no localStorage do navegador do usuário
-  // Para persistência compartilhada, seria necessário uma base de dados externa
-  return {
-    statusCode: 200,
-    body: JSON.stringify([])
-  };
-};
